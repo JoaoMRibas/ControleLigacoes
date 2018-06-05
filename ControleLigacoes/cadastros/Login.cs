4e836e23c65b88ff079af2da97ca1072134aacde
João <Joo@DESKTOP-UA1IOOT>
2018-06-05 11:57:04 -0300

    Na verdade é só a tela de login sem nada programado mas commit mesmo assim

---
+++ ControleLigacoes/cadastros/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleLigacoes.cadastros
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            LigacoesContext context = new LigacoesContext;
+                var usuarios = from DbSet<Usuario> Usuarios;
+        }
+    }
+}
